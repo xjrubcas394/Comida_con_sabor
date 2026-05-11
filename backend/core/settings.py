7b32584e6410905b835e1a75ae9c114a8238c587
xjrubcas394 <xjrubcas394@ieshnosmachado.org>
2026-05-11 19:40:57 +0200

    Corregir rutas

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,8 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
